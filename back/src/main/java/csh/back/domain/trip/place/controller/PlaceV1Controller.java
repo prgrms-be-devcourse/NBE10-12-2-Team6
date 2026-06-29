58fe5b63f824b9ed341c94a8307b1306dcc9db39
devho <jshgold93@gmail.com>
2026-06-29 17:41:17 +0900

    feat/13/dev-RequestDto,ResponseDto 최초 생성

--- back/src/main/java/csh/back/domain/trip/place/controller/PlaceV1Controller.java
+++ back/src/main/java/csh/back/domain/trip/place/controller/PlaceV1Controller.java
@@ -1,12 +1,17 @@
 package csh.back.domain.trip.place.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/trip/")
+@RequestMapping("/api/v1/trips/")
 @RequiredArgsConstructor
 public class PlaceV1Controller {
+
+    @RequestMapping("/{tripId}/wish-places")
+    public void saveWishPlace(@RequestBody @PathVariable String tripId) {}
 
 }
--- back/src/main/java/csh/back/domain/trip/place/dto/PlaceRequestDto.java
+++ back/src/main/java/csh/back/domain/trip/place/dto/PlaceRequestDto.java
@@ -1,4 +1,12 @@
 package csh.back.domain.trip.place.dto;
 
 public class PlaceRequestDto {
+    public record SaveRequest(String name,
+                              String category,
+                              String address,
+                              String kakaoPlaceId,
+                              String kakaoUrl
+    ) {
+
+    }
 }
