f559b54ca77f0a91d1418ad453f4a666b1ad53c4
devho <jshgold93@gmail.com>
2026-06-29 23:55:53 +0900

    feat/22/dev-장소 후보 전체 조회 기능 개발

--- back/src/main/java/csh/back/domain/trip/place/repository/PlaceRepository.java
+++ back/src/main/java/csh/back/domain/trip/place/repository/PlaceRepository.java
@@ -3,5 +3,8 @@ package csh.back.domain.trip.place.repository;
 import csh.back.domain.trip.place.entity.TripPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceRepository extends JpaRepository<TripPlace, Long> {
+    List<TripPlace> findAllByTripId(Long tripId);
 }
--- back/src/main/java/csh/back/domain/trip/place/service/PlaceService.java
+++ back/src/main/java/csh/back/domain/trip/place/service/PlaceService.java
@@ -1,12 +1,27 @@
 package csh.back.domain.trip.place.service;
 
+import csh.back.domain.trip.place.dto.PlaceResponseDto;
+import csh.back.domain.trip.place.entity.TripPlace;
+import csh.back.domain.trip.place.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class PlaceService {
+    private final PlaceRepository placeRepository;
+
+    public List<PlaceResponseDto.PlaceFindItem> findWishPlaces(Long tripId) {
+        List<TripPlace> tripPlaces = placeRepository.findAllByTripId(tripId);
+        return tripPlaces
+                .stream()
+                .map(PlaceResponseDto.PlaceFindItem::from)
+                .toList();
+    }
+
 }
 
