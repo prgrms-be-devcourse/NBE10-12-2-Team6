1d193222cf88d18a576ef577fd2c822b6ef07c99
devHo <jshgold93@gmail.com>
2026-06-29 15:57:23 +0900

    feat/14/ci-git hook 설정파일 추가 (#16)

--- back/src/main/java/csh/back/domain/member/repository/TemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package csh.back.domain.member.repository;
-
-import csh.back.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateRepository extends JpaRepository<Member, Long> {
-}
--- back/src/main/java/csh/back/domain/trip/group/repository/TemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package csh.back.domain.trip.group.repository;
-
-import csh.back.domain.trip.group.entity.TripGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemplateRepository extends JpaRepository<TripGroup, Long> {
-}
