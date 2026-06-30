42f8451e09390e9728096fdd2270136aec5a75a2
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-06-30 21:40:25 +0900

    feat/43/feat- 모임방 생성에 대한 예외처리

--- back/src/main/java/csh/back/domain/trip/group/controller/TripGroupV1Controller.java
+++ back/src/main/java/csh/back/domain/trip/group/controller/TripGroupV1Controller.java
@@ -8,11 +8,14 @@ import csh.back.domain.trip.group.dto.response.TripGroupResponse;
 import csh.back.domain.trip.group.exception.NotFoundException;
 import csh.back.domain.trip.group.service.TripGroupService;
 import csh.back.global.dto.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "여행 모임방", description = "여행 모임 관리 API")
 @RestController
 @RequestMapping("/trips")
 @RequiredArgsConstructor
@@ -21,6 +24,8 @@ public class TripGroupV1Controller {
 	private final TripGroupService tripGroupService;
 	private final MemberRepository memberRepository;
 
+	//Swagger 문서 표시
+	@Operation(summary = "모임방 목록 조회(로그인한 사용자 기준)")
 	//모임방 조회
 	@GetMapping()
 	public ResponseData<List<TripGroupResponse>> getAllGroups(
@@ -31,6 +36,8 @@ public class TripGroupV1Controller {
 		return new ResponseData<>(200, tripGroupService.getGroups(ownerId));
 	}
 
+	//Swagger 문서 표시
+	@Operation(summary = "모임방 생성")
 	//모임방 생성
 	@PostMapping()
 	public ResponseData<TripGroupResponse> saveGroup(
@@ -42,6 +49,8 @@ public class TripGroupV1Controller {
 		return new ResponseData<>(201, tripGroupService.writeGroup(request, owner));
 	}
 
+	//Swagger 문서 표시
+	@Operation(summary = "상세 모임방 조회")
 	//모임방 상세페이지 조회
 	@GetMapping("/{groupId}")
 	public ResponseData<TripGroupResponse> getGroupDetail(
@@ -51,7 +60,9 @@ public class TripGroupV1Controller {
 //		Long ownerId = userDetails.getMember().getId(); // id만 추출
 		return new ResponseData<>(200, tripGroupService.getGroupDetail(groupId, ownerId));
 	}
-//
+
+	//Swagger 문서 표시
+	@Operation(summary = "상세 모임방 수")
 	//모임방 상세 수정 - name
 	@PatchMapping("/{groupId}")
 	public ResponseData<TripGroupResponse> modifyGroupName(
