741a05fbdc4643af1931f5872e51a912589920c0
developerapp0721@gmail.com <developerapp0721@gmail.com>
2026-06-30 17:31:10 +0900

    feat: 여행 타임라인 시간 구간 CRUD API 구현

--- back/src/main/java/csh/back/domain/trip/member/repository/TripMemberRepository.java
+++ back/src/main/java/csh/back/domain/trip/member/repository/TripMemberRepository.java
@@ -4,4 +4,10 @@ import csh.back.domain.trip.member.entity.TripMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
+
+    //해당 사용자가 특정 여행 모임의 멤버인지 확인
+    boolean existsByTripGroupIdAndOwnerId(Long tripId, Long memberId);
+
+    //해당 사용자가 특정 여행 모임의 방장인지 확인
+    boolean existsByTripGroupIdAndOwnerIdAndIsAdminTrue(Long tripId, Long memberId);
 }
