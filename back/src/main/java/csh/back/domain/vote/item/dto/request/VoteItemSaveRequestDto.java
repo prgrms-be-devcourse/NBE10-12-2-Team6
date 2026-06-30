f9963ee81983e926e229e49ab900a11e53f0b0be
devho <jshgold93@gmail.com>
2026-06-30 17:19:16 +0900

    feat/37/dev-vote 초기 설정

--- back/src/main/java/csh/back/domain/vote/item/dto/request/VoteItemSaveRequestDto.java
+++ back/src/main/java/csh/back/domain/vote/item/dto/request/VoteItemSaveRequestDto.java
@@ -1,6 +1,7 @@
 package csh.back.domain.vote.item.dto.request;
 
-public record VoteItemSaveRequestDto(Long voteId,
-                                     Long placeId,
-                                     Long testId) {
+public record VoteItemSaveRequestDto(
+        Long voteId,
+        Long placeId) {
+
 }
