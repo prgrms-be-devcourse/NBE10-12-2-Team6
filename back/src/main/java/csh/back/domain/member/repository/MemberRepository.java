51feffa57d8de5c2d1ac8964be804b7734b8fa88
A-Light-Shining-in-Darkness <kor2002rea@naver.com>
2026-06-30 11:29:12 +0900

    feat/5/dev-회원가입API 구현

--- back/src/main/java/csh/back/domain/member/repository/MemberRepository.java
+++ back/src/main/java/csh/back/domain/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package csh.back.domain.member.repository;
 import csh.back.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 회원 레포지토리
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 이메일 중복 체크
     boolean existsByEmail(String email);
 }
