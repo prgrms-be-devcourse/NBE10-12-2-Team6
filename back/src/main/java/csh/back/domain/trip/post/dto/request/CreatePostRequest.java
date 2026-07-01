179fbba86454ea2464d888af4665f129067d105f
kang <zwhstn@gmail.com>
2026-07-01 12:03:01 +0900

    feat/fix PostController swagger 추가/48

--- back/src/main/java/csh/back/domain/trip/post/dto/request/CreatePostRequest.java
+++ back/src/main/java/csh/back/domain/trip/post/dto/request/CreatePostRequest.java
@@ -1,8 +1,16 @@
 package csh.back.domain.trip.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreatePostRequest(
+
+        @Schema(description = "게시글 내용", example = "부산 여행 시작!")
         String content,
+
+        @Schema(description = "위치", example = "부산 광안리")
         String location,
+
+        @Schema(description = "이미지 여부", example = "true")
         Boolean isImg
 ) {
 }
