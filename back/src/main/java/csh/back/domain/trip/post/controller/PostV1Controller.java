686038bea10043cd5b0e4d51afc91aa14fcadb47
DONGYUN <zwhstn@gmail.com>
2026-07-02 15:03:27 +0900

    fix/Post API 주석 리펙토링/69 (#71)

--- back/src/main/java/csh/back/domain/trip/post/controller/PostV1Controller.java
+++ back/src/main/java/csh/back/domain/trip/post/controller/PostV1Controller.java
@@ -4,6 +4,7 @@ import csh.back.domain.trip.post.dto.request.CreatePostRequest;
 import csh.back.domain.trip.post.dto.request.UpdatePostRequest;
 import csh.back.domain.trip.post.dto.response.PostResponse;
 import csh.back.domain.trip.post.service.PostService;
+import csh.back.global.annotation.ApiV1;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,13 +15,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/trips/{tripId}/posts") //기존 /post가 아닌 여행 API 명세서에 따른 하위 URL로 수정
+@ApiV1
+@RequestMapping("/trips/{tripId}/posts")
 @Tag(name = "Post", description = "게시글 API")
-public class PostController {
+public class PostV1Controller {
 
     private final PostService postService;
 
-    @PostMapping("/{tripMemberId}/{timelineId}") //여행 맴버 및 여행 ID 조회를 통한 검증
+    @PostMapping("/{tripMemberId}/{timelineId}")
     @Operation(summary = "게시글 생성", description = "새로운 게시글을 생성합니다.")
     public PostResponse create(
 
@@ -36,8 +38,8 @@ public class PostController {
             @RequestBody CreatePostRequest request
     ) {
         return postService.create(
-                tripId, // trip의 하위 개념으로 옮겼으므로 tripID 역시 참조해야함
-                tripMemberId, // trip의 하위 개념으로 옮겼으므로 tripMemberId 역시 참조해야함
+                tripId,
+                tripMemberId,
                 timelineId,
                 request
         );
@@ -46,7 +48,7 @@ public class PostController {
     @GetMapping("/{postId}")
     @Operation(summary = "게시글 조회", description = "게시글 단건 조회")
     public PostResponse getPost(
-            @PathVariable Long tripId, // trip의 하위 개념으로 옮겼으므로 tripID를 참조
+            @PathVariable Long tripId,
             @PathVariable Long postId
     ) {
         return postService.getPost(tripId, postId);
