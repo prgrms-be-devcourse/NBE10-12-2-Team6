3420d02ed5a68ead0b735353cd6db36bf810c511
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-07-02 14:10:08 +0900

    feat/4/dev - 공용 api 어노테이션 추가

--- back/src/main/java/csh/back/domain/trip/member/controller/TripMemberV1Controller.java
+++ back/src/main/java/csh/back/domain/trip/member/controller/TripMemberV1Controller.java
@@ -1,12 +1,14 @@
 package csh.back.domain.trip.member.controller;
 
 import csh.back.domain.trip.group.service.TripGroupService;
+import csh.back.global.annotation.ApiV1;
 import csh.back.global.dto.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@ApiV1
 @Tag(name = "여행 맴버", description = "여행 멤버 관리 API")
 @RestController
 @RequestMapping("/trip_member")
