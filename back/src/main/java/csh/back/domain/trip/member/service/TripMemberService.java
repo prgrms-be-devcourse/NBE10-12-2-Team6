39bb90f84b51d1279f967af22b7290e6772d0cb7
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-07-07 09:49:49 +0900

    feat/122/hotfix - 로그인시 초대 코드가 없어질 경우 로그인 되도록

--- back/src/main/java/csh/back/domain/trip/member/service/TripMemberService.java
+++ back/src/main/java/csh/back/domain/trip/member/service/TripMemberService.java
@@ -18,28 +18,21 @@ public class TripMemberService {
 	private final TripGroupRepository tripGroupRepository;
 	private final TripMemberRepository tripMemberRepository;
 	private final MemberRepository memberRepository;
-
+	//FIXME 나중에 boolean타입으로 넣어서.. 프론트에서 이걸 구분되도록...
 	// joinCode 조회 후 멤버 추가
 	@Transactional
 	public void createJoinMember(String joinCode, Long memberId) {
-		TripGroup tripGroup = tripGroupRepository.findByJoinCode(joinCode)
-				.orElseThrow(() -> new NotFoundException("존재하지 않는 모임방"));
+		tripGroupRepository.findByJoinCode(joinCode).ifPresent(tripGroup -> {
 
-		Member member = memberRepository.findById(memberId)
-				.orElseThrow(() -> new NotFoundException("존재하지 않는 유저"));
+			Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException("존재하지 않는 유저"));
 
-		if (tripMemberRepository.existsByTripGroupIdAndMemberId(tripGroup.getId(), member.getId())) {
-			//이미 가입되었으면 성공으로 save 없이 통과
-			// 혹시 모를 서버 중단으로 save가 2번 이상 될 수 있는 점을 예외처리 하기 위해 추가함
-			return;
-		}
+			if (tripMemberRepository.existsByTripGroupIdAndMemberId(tripGroup.getId(), member.getId())) {
+				//이미 가입되었으면 성공으로 save 없이 통과
+				// 혹시 모를 서버 중단으로 save가 2번 이상 될 수 있는 점을 예외처리 하기 위해 추가함
+				return;
+			}
 
-		tripMemberRepository.save(
-				TripMember.builder()
-						.member(member)
-						.tripGroup(tripGroup)
-						.isAdmin(false)
-						.build()
-		);
+			tripMemberRepository.save(TripMember.builder().member(member).tripGroup(tripGroup).isAdmin(false).build());
+		});
 	}
 }
